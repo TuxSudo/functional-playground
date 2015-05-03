03153a7a6b3d0976f05fb7dea558321c60aa0c21
Jared Anderson <jared@tuxsudo.com>
2015-05-03 17:32:54 -0600

    removed unneeded parens from curry

--- src/curry.js
+++ src/curry.js
@@ -1,5 +1,5 @@
 export default function curry(fn) {
     return function (arg) {
-        return fn.length <= 1 ? fn(arg) : curry( (fn.bind(null, arg)) );
+        return fn.length <= 1 ? fn(arg) : curry( fn.bind(null, arg) );
     };
 }

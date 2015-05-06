cba5b8b858867f9449f79085c1664f546835064b
Jared Anderson <jared@tuxsudo.com>
2015-05-05 18:29:53 -0600

    shorter

--- src/compose.js
+++ src/compose.js
@@ -1,8 +1,6 @@
 export default function (...fns) {
 
-    var reversed = fns.reverse();
-
-    return x => reversed.reduce( (lastval, thisfn) => thisfn( lastval ), x );
+    return x => fns.reduceRight( (lastval, thisfn) => thisfn( lastval ), x );
 
 }
 
